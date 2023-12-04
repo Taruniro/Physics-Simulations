e10d821635feed79e3ba035adae49e03b77669ad
Phuphinyo L <phuphinyol@gmail.com>
2023-12-04 15:25:40 +0700

    Update Description

--- IYPT2023/[MATLAB] IYPT2023 Siren Audio Simulation.m
+++ IYPT2023/[MATLAB] IYPT2023 Siren Audio Simulation.m
@@ -1,18 +1,20 @@
-clear;clc;
-r = 2;           % radius of siren holes = 2 mm
-ro = 2;          % radius of orifice
-h = 50;          % radius of concentric circle = 50 mm
-n = 16;          % number of holes along concentric circle
-l = 2*pi*h/n;    % distance between 2 consecutive siren holes
-s_rpm = 1200;     % rotating speed of siren disc (rpm)
-s_rps = 1200/60;  % rotating speed of siren disc (rps)
-Fs = 44100;
+clear;clc;          % Clear calculation
+r = 2;              % Radius of siren holes
+ro = 2;             % Radius of air jet orifice
+h = 50;             % Radius of concentric circle
+n = 16;             % Number of holes along concentric circle (equally distanced)
+l = 2*pi*h/n;       % Distance between 2 consecutive siren holes
+s_rpm = 1200;       % Rotating speed of siren disc (rpm)
+s_rps = 1200/60;    % Rotating speed of siren disc (rps)
+
+Fs = 44100;         %
 dt = 1/Fs;
 step = 2*pi*h*s_rps*dt;
 dmax = 2*pi*h*100;
 d = 0:step:dmax;
 n = length(d);
 a = zeros(n,1);
+
 for i=1:n
     a(i) = area1(r,l,d(i));
 end
